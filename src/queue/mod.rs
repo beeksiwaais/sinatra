68f707a221fd9452d22820e5bd3e3480ca18b6c6
Beeksi Waais <181389+beeksiwaais@users.noreply.github.com>
2025-12-23 23:59:47 +0100

    remove job from mod.rs because igt is not needed outside of queue

--- src/queue/mod.rs
+++ src/queue/mod.rs
@@ -3,7 +3,6 @@ pub mod orchestrator;
 pub mod redis_queue;
 pub mod worker;
 
-pub use job::{SegmentJob, VideoStatus};
 pub use orchestrator::enqueue_video;
 pub use redis_queue::{QueueError, RedisQueue};
 pub use worker::{WorkerPool, WORKERS_COUNT};
